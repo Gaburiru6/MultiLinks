b46e86052810a9a23b03887b10c43ff15def87b5
Vitor Paulino <vitorpaulinog@gmail.com>
2024-09-20 18:21:56 -0300

    First Commit

---
+++ src/main/java/com/example/MultiLinks/MultiLinksApplication.java
@@ -0,0 +1,13 @@
+package com.example.MultiLinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiLinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiLinksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MultiLinks/MultiLinksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MultiLinks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiLinksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
