43c9b7a9f90bfa76ec10147b07eab42fd03b519d
Gabriel Santos Soares <gs5560752@gmail.com>
2024-09-23 13:09:38 -0300

    Add: User mapping
    Add: User id

--- src/main/java/com/example/MultiLinks/Model/User.java
+++ src/main/java/com/example/MultiLinks/Model/User.java
@@ -1,12 +1,27 @@
 package com.example.MultiLinks.Model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.*;
+
+import java.util.UUID;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "User")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
     private String nome;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String senha;
 }
