5e2ee08491a7c69f4ef97bb9909857a19beff024
JeaneMirele <jeanemirele7@gmail.com>
2024-09-30 11:50:49 -0300

    Add: UrlController and UserController
    Add crud Endpoints

--- src/main/java/com/example/MultiLinks/Controller/UrlController.java
+++ src/main/java/com/example/MultiLinks/Controller/UrlController.java
@@ -1,4 +1,51 @@
 package com.example.MultiLinks.Controller;
 
+
+import com.example.MultiLinks.Model.Url;
+import com.example.MultiLinks.Repositories.UrlRepository;
+import com.example.MultiLinks.Services.UrlService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@RestController
+@RequestMapping("/api/url")
 public class UrlController {
+     @Autowired
+        private UrlService urlService;
+
+     @Autowired
+        private UrlRepository urlRepository;
+
+    @PostMapping("/encurtadorUrl")
+    public ResponseEntity<String> EncurtarUrl(@RequestBody String urlOriginal){
+        String UrlEncurtada = urlService.EncurtarUrl(urlOriginal);
+        return ResponseEntity.ok(UrlEncurtada);
+    }
+
+    @GetMapping("/{urlEncurtada}")
+    public ResponseEntity redirecionarUrl(@PathVariable("urlEncurtada") String urlEncurtada){
+
+       Url url= urlRepository.findByUrlEncurtada(urlEncurtada);
+        HttpHeaders headers= new HttpHeaders();
+        headers.setLocation(URI.create(url.getUrlOriginal()));
+
+          if(url!=null){
+
+              return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
+
+          }
+
+
+         return ResponseEntity.notFound().build();
+    }
+
 }
+
+
